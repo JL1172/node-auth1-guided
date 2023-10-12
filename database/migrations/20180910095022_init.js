f0b05e372ca2e4d2933205aecee281eaa7bf974a
Jacob Lang <jacoblang72@comcast.net>
2023-10-12 17:42:50 -0400

    wipwip

--- database/migrations/20180910095022_init.js
+++ database/migrations/20180910095022_init.js
@@ -1,12 +1,21 @@
-exports.up = function (knex) {
+
+
+exports.up = async function (knex) {
   return knex.schema
     .createTable("users", tbl => {
       tbl.increments()
       tbl.string("username", 128).notNullable().unique().index()
       tbl.string("password", 256).notNullable()
     })
+    .createTable("sessions",table => {
+      table.string("session_id").primary();
+      table.json("session").notNullable();
+      table.datetime("sessions_expired").notNullable();
+    })
 }
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users")
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("users"); 
+  await knex.schema.dropTableIfExists("sessions");
 }
